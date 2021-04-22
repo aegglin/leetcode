b40f1f4de571886bafa17a2aeeaf29720281674e
aegglin <uefadude@gmail.com>
2021-04-22 17:30:05 -0400

    Update Employees Earning More Than Their Managers.sql
    Added comment as description of problem.

--- sql/Employees Earning More Than Their Managers.sql
+++ sql/Employees Earning More Than Their Managers.sql
@@ -1,3 +1,8 @@
+/* 
+  The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+  Fun little question with a self-join necessary because all the information is contained in one table. 
+*/
+
 SELECT e1.Name as Employee
 FROM Employee e1 JOIN Employee e2 ON e1.ManagerId=e2.Id
 WHERE e1.Salary > e2.Salary
