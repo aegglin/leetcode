c7a80db03b3c9ec9c31ee2e17a626dc911385ba2
aegglin <uefadude@gmail.com>
2021-04-23 09:19:51 -0400

    Update Employees Earning More Than Their Managers.sql

--- sql/Employees Earning More Than Their Managers.sql
+++ sql/Employees Earning More Than Their Managers.sql
@@ -3,6 +3,6 @@
   Fun little question with a self-join necessary because all the information is contained in one table. 
 */
 
-SELECT e1.Name as Employee
+SELECT e1.Name AS Employee
 FROM Employee e1 JOIN Employee e2 ON e1.ManagerId=e2.Id
 WHERE e1.Salary > e2.Salary
