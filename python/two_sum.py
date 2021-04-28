db37f9191cc27dedbad6338364951c32f8502937
aegglin <uefadude@gmail.com>
2021-04-28 14:17:34 -0400

    Updated title according to PEP8

---
+++ python/two_sum.py
@@ -0,0 +1,12 @@
+def twoSum(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+     rtype: List[int]
+     """
+        
+    for i, x in enumerate(nums):
+        other_nums = nums[i+1:]
+        for other_index, other_num in enumerate(other_nums):
+             if x + other_num == target:
+                return [i, other_index + (len(nums)-len(other_nums))]
